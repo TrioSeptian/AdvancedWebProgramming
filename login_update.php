3ecd199a65e5d1baf4eb92d10fd8e1be907c4c7e
TrioSeptian <trioseptian24@gmail.com>
2017-09-28 23:45:15 +0700

    LEVEL 2 - Query to Read Id’s (part 1)

--- login_update.php
+++ login_update.php
@@ -1,3 +1,14 @@
+<?php
+
+include "db.php";
+
+$query = "SELECT * FROM users";
+$result = mysqli_query($koneksi, $query);
+
+if (!$result){
+	die ('Query failed' . mysqli_error($koneksi));
+}
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -18,7 +29,12 @@
 					</div>
 					<div class="form-group">
 						<select name="id" class="form-control">
-							<option value="1">1</option>
+							<?php
+							while($row = mysqli_fetch_assoc($result)){
+								$id = $row['id'];
+								echo "<option value='$id'>$id</option>";
+							}
+							?>
 						</select>
 					</div>
 					<input type="submit" name="submit" value="UPDATE" class="btn btn-primary" />
